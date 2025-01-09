f85b7c04f92ef1cb233a951d5b916fdd6f3f9381
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2025-01-09 12:14:41 +0100

    feat: add font family extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 
 
 import StarterKit from '@tiptap/starter-kit';
+import FontFamily from '@tiptap/extension-font-family';
+import TextStyle from '@tiptap/extension-text-style';
 import TaskItem from '@tiptap/extension-task-item';
 import TaskList from '@tiptap/extension-task-list';
 import Image from '@tiptap/extension-image';
@@ -53,6 +55,8 @@ export const Editor = () => {
         extensions: [
             StarterKit,
             Underline,
+            FontFamily,
+            TextStyle,
             Image,
             ImageResize,
             TaskList,
@@ -83,8 +87,11 @@ export const Editor = () => {
         </table>
         <br />
         <p>This is a basic example of implementing images. Drag to re-order.</p>
-        <img src="https://placehold.co/800x400" />
         <img src="https://placehold.co/800x400/6A00F5/white" />
+
+        <p><span style="font-family: serif">Serious people use serif fonts anyway.</span></p>
+        <p><span style="font-family: monospace">The cool kids can apply monospace fonts aswell.</span></p>
+        <p><span style="font-family: 'Exo 2'">TipTap even can handle exotic fonts as Exo 2.</span></p>
       `,
     })
 
