8ddaf2f10b7426af939d769a1d61d52f357facec
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2025-02-02 21:45:22 +0100

    feat: implement user avatars

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -20,7 +20,10 @@ import {
 
 import { DocumentInput } from './document-input';
 import { Bold, File, FileJson, FilePen, FilePlus2, FileText, Globe, Italic, Printer, Redo2, RemoveFormatting, Strikethrough, TextIcon, Trash, Underline, Undo2 } from 'lucide-react';
+
 import { BsFilePdf } from 'react-icons/bs';
+import { Avatars } from './avatars';
+
 import { useEditorStore } from '@/store/use-editor-store';
 
 
@@ -199,6 +202,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="flex gap-3 items-center pl-6">
+                <Avatars />
                 <OrganizationSwitcher
                     afterCreateOrganizationUrl={'/'}
                     afterLeaveOrganizationUrl="/"
